59c0736c24f9496763cfb5bae351607fa5f60fd1
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-12-05 11:05:21 +0700

    add param for log dir and weight dir

--- train.py
+++ train.py
@@ -15,12 +15,13 @@ parser.add_argument('--src_vocab', required=True)
 parser.add_argument('--tgt_vocab', required=True)
 parser.add_argument('--train_file', required=True)
 parser.add_argument('--test_file', required=True)
+parser.add_argument('--model_config')
 parser.add_argument('--batch_size', default=32, type=int)
 parser.add_argument('--learning_rate', default=1e-3, type=float)
 parser.add_argument('--num_epoch', default=10, type=int)
 parser.add_argument('--device', default='cpu')
-parser.add_argument('--log_file', default='log.txt')
-parser.add_argument('--model_config')
+parser.add_argument('--log_dir', default='logs')
+parser.add_argument('--weight_dir', default='weight')
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -47,7 +48,12 @@ if __name__ == "__main__":
     model = Model(src_vocab_len, tgt_vocab_len, **config)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=(0.9, 0.98), eps=1e-9)
     sched = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=len(train_dl))
-    trainner = Trainer(model, optimizer, train_dl, test_dl, device=args.device, scheduler=sched)
+    trainner = Trainer(
+        model, optimizer, train_dl, test_dl, 
+        device=args.device, scheduler=sched,
+        log_dir=args.log_dir,
+        weight_dir=args.weight_dir
+    )
 
     print("Start training")
     trainner.train(args.num_epoch)
