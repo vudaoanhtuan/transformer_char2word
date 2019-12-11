6fdcd6a35e38a4acc9c51f6097719c59bacf410c
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-12-11 15:17:24 +0700

    add predict mask

--- decode.py
+++ decode.py
@@ -23,3 +23,24 @@ def greedy_decode(model, tokenizer, inp, max_len=100):
     seq = hyp.numpy().tolist()[0]
     sent = [tokenizer.tgt_itos[x] for x in seq[1:i]]
     return ' '.join(sent)
+
+
+def greedy_decode_mask(model, tokenizer, inp):
+    model.eval()
+    src = tokenizer.tokenize_src(inp)
+    src = torch.tensor([src]).long()
+    tgt_inp = tokenizer.tokenize_tgt(inp)
+    tgt_inp = torch.tensor([tgt_inp]).long()
+
+    memory = model.encode(src)
+
+    output = model.decode(
+        tgt_inp, memory
+    ) # BxTxV
+
+    seq = output.argmax(dim=-1)[0]
+    seq = seq.detach().numpy().tolist()
+
+    sent = [tokenizer.tgt_itos[x] for x in seq[1:-1]]
+
+    return ' '.join(sent)
