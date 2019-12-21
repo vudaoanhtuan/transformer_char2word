2b2b03656c3e117ad1f88ab457d8ff81c2b39878
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-12-21 10:43:11 +0700

    add transform word

--- utils/word_transform.py
+++ utils/word_transform.py
@@ -2,77 +2,46 @@ import numpy as np
 import visen
 
 CHAR_LIST = "abcdefghijklmnopqrstuvwxyzàáâãèéêìíòóôõùúýăđĩũơưạảấầẩẫậắằẳẵặẹẻẽếềểễệỉịọỏốồổỗộớờởỡợụủứừửữựỳỵỷỹ"
+VOWEL_LIST = list("aeiouàáâãèéêìíòóôõùúýăđĩũơưạảấầẩẫậắằẳẵặẹẻẽếềểễệỉịọỏốồổỗộớờởỡợụủứừửữựỳỵỷỹ")
+CONSONANT_LIST = list("bcdfghjklmnpqrstvwxyz")
 
 def delete_char(word, num_char=1):
     word = [c for c in word]
     n = len(word)
-    if n>2:
-        pos = np.random.choice(range(1,n), num_char, replace=False)
-        for i in pos:
-            word[i] = ''
+    num_char = min(num_char, n)
+    pos = np.random.choice(range(1,n), num_char, replace=False)
+    for i in pos:
+        word[i] = ''
+    word = ''.join(word)
+    return word
+
+def substitute_char(word, num_char=1):
+    word = [c for c in word]
+    n = len(word)
+    num_char = min(num_char, n)
+    pos = np.random.choice(range(1,n), num_char, replace=False)
+    for i in pos:
+        c = word[i]
+        if c in VOWEL_LIST:
+            c = np.random.choice(VOWEL_LIST)
+        else:
+            c = np.random.choice(CONSONANT_LIST)
+        word[i] = c
     word = ''.join(word)
     return word
 
 
-def short_word(word):
-    sw_dict = {
-        'không': ['k', 'ko', 'k0', 'kg', 'hk'],
-        'vậy': ['z', 'zậy'],
-        'gì': ['j', 'gj'],
-    }
+def transform_sentence(sent, p_transform=0.4, p_del=0.1, p_sub=0.9, p_tf_word={'del':0.5, 'sub':0.5}):
+    words = sent.split()
+    tf_type = np.random.choice([0,1,2], len(words), p=[1-p_transform, p_transform*p_del, p_transform*p_sub])
+    for i,t in enumerate(tf_type):
+        if t==1:
+            words[i] = ''
+        elif t==2:
+            n_iter = np.random.randint(1,3)
+            for _ in range(n_iter):
+                func = np.random.choice([delete_char, substitute_char], p=[p_tf_word['del'], p_tf_word['sub']])
+                words[i] = func(words[i])
+    words = [w for w in words if w!='']
+    return ' '.join(words)
 
-def get_teen_code(word):
-    def vz(word):
-        # vậy -> zậy
-        word = word.replace('v', 'z')
-        return word
-    
-    def ij(word):
-        # vui -> vuj
-        word = word.replace('i', 'j')
-        return word
-
-    def qw(word):
-        # qua -> wua
-        word = word.replace('q', 'w')
-        return word
-
-    def quw(word):
-        # qua -> wa
-        word = word.replace('qu', 'w')
-        return word
-    
-    def ng(word):
-        # không -> khôg
-        if word.endswith('ng'):
-            word = word[:-2] + 'g'
-        return word
-    
-    for func in [vz, ij, quw, qw, ng]:
-        word = func(word)
-    
-    return word
-
-def remove_tone(word):
-    word = visen.remove_tone(word)
-    return word
-
-def get_enter_code(word):
-    code = visen.format.get_enter_code(word)
-    return code
-
-def get_typo(word):
-    word = get_enter_code(word)
-    num_del = [1,2]
-    pc = [0.85, 0.15]
-    if len(word) < 5:
-        num_del = [1,1]
-    n = np.random.choice(num_del, p=pc)
-    word = delete_char(word, n)
-    return word
-
-def gen_wrong_word(word):
-    func = [get_typo, get_teen_code, remove_tone]
-    func = np.random.choice(func)
-    word = func(word)
-    return word
