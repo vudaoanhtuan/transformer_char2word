3eb231903695237879180b969f6f295760c0f1d4
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-12-22 17:23:17 +0700

    edit maskdataset

--- dataset.py
+++ dataset.py
@@ -7,6 +7,7 @@ import pandas as pd
 from tqdm import tqdm
 
 from utils.preprocess_utils import pad_sequences
+from utils.word_transform import transform_sentence
 
 
 class Dataset(data.Dataset):
@@ -37,42 +38,35 @@ class MaskDataset(data.Dataset):
     def __init__(self, file_path, tokenizer, src_pad_len=200, tgt_pad_len=50, use_mask=True):
         self.tokenizer = tokenizer
         self.use_mask = use_mask
-        self.src_pad_len = src_pad_len
-        self.tgt_pad_len = tgt_pad_len
 
-        df = pd.read_csv(file_path, sep='\t', names=['src', 'tgt'])
+        with open(file_path) as f:
+            self.corpus = f.read().split('\n')[:-1]
 
-        tokens = [tokenizer.tokenize(str(x.src), str(x.tgt)) for i, x in tqdm(df.iterrows())]
-        self.src = [x[0] for x in tokens]
-        self.tgt = [x[1] for x in tokens]
-
-        self.pad_value = self.tokenizer.pad
-        self.mask_value = self.tokenizer.mask
+        self.src = None
+        self.tgt = [tokenizer.tokenize_tgt(x) for x in tqdm(self.corpus)]
 
         self.src_vocab_len = len(self.tokenizer.src_stoi)
         self.tgt_vocab_len = len(self.tokenizer.tgt_stoi)
+        self.src_pad_len = src_pad_len
+        self.tgt_pad_len = tgt_pad_len
 
-    def mask_item(self, x,
-        pad_value, mask_value, vocab_len,
-        pc_choice=0.15, pc_mask=0.8, pc_other=0.1, pc_keep=0.1):
-        n_mask = int(pc_choice*len(x))
+        self.regenerate_source()
 
-        if n_mask > 0:
-            mask_ix = np.random.choice(range(0, len(x)), n_mask)
-            mask_v = np.random.choice([0,1,2], n_mask, p=[pc_mask, pc_other, pc_keep])
-
-            x = x.copy()
-            for i,v in zip(mask_ix, mask_v):
-                if v==0:
-                    x[i] = mask_value
-                elif v==1:
-                    x[i] = np.random.randint(5, vocab_len)
-            return x
-
+    def random_mask_tgt(self, x, pc_mask=0.3):
+        mask = np.random.choice([0,1], len(x), p=[1-pc_mask, pc_mask])
+        x = np.where(mask==1, self.tokenizer.mask, x)
         return x
 
+    def regenerate_source(self):
+        del self.src
+        self.src = []
+        for sent in tqdm(self.corpus):
+            sent = transform_sentence(sent)
+            tokens = self.tokenizer.tokenize_src(sent)
+            self.src.append(tokens)
+
     def __len__(self):
-        return len(self.src)
+        return len(self.corpus)
 
     def __getitem__(self, index):
         src = self.src[index]
@@ -80,8 +74,7 @@ class MaskDataset(data.Dataset):
         tgt_lbl = self.tgt[index][1:]
 
         if self.use_mask:
-            src = self.mask_item(src, self.pad_value, self.mask_value, self.src_vocab_len)
-            tgt_inp = self.mask_item(tgt_inp, self.pad_value, self.mask_value, self.tgt_vocab_len)
+            tgt_inp = self.random_mask_tgt(tgt_inp)
 
         src = pad_sequences([src], maxlen=self.src_pad_len, value=self.tokenizer.pad, padding='post')[0]
         tgt_inp = pad_sequences([tgt_inp], maxlen=self.tgt_pad_len, value=self.tokenizer.pad, padding='post')[0]
