ad3d8b1d095f515927956446cbc9c523c06bb38c
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2020-06-02 14:42:12 +0700

    rm dataset

--- predict.py
+++ predict.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 
 from model import Model
-from dataset import Dataset, MaskDataset
 from tokenizer import Tokenizer, load_tokenizer
 from decode import greedy_decode, BeamDecode
 
